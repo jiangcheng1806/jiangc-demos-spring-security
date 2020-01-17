b6d23d519e1afe2ca23e05f7f131ada85f10f434
jiangcheng <jiangcheng05@sinochem.com>
2020-01-17 17:57:38 +0800

    新建一个可以springboot项目 可以使用 lombok 方便的打印日志

---
+++ src/test/java/org/jiangc/demos/springsecurity/LogPrintTest.java
@@ -0,0 +1,17 @@
+package org.jiangc.demos.springsecurity;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Slf4j
+public class LogPrintTest {
+
+    //private static final Logger logger = LoggerFactory.getLogger(LogPrintTest.class);
+
+    public static void main(String[] args) {
+        //logger.info("s");
+
+        log.info("ss");
+    }
+}
